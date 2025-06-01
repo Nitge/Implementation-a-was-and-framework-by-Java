045cc7675d8d53b948371b43238af37d4ce7b300
Nitge <42697796+Nitge@users.noreply.github.com>
2025-06-01 22:37:17 +0900

    main에서 포트 설정 추가

--- VanillaWAS/src/main/java/ServerMain.java
+++ VanillaWAS/src/main/java/ServerMain.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 public class ServerMain {
-    public static final int PORT = 80;
+    public static int PORT = 12345;
 
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, IOException {
         DispatcherServlet dispatcherServlet = new DispatcherServlet();
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(dispatcherServlet);
+        if(args.length > 0) {
+            PORT = Integer.parseInt(args[0]);
+        }
         new HttpServer(PORT, servletManager).start();
     }
 }
